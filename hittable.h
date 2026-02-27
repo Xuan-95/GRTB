7f22675f627e98d420bd506176f382e71ae83794
Xuan-95 <x1naselli@gmail.com>
2026-02-27 10:38:58 +0100

    add hittable "abstract" struct

---
+++ hittable.h
@@ -0,0 +1,24 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+#include "vector3d.h"
+
+typedef struct Hittable Hittable;
+
+typedef struct {
+    Point3D p;
+    Vector3D normal;
+    double t;
+    int front_face;
+} HitRecord;
+
+struct Hittable {
+    int (*hit)(Hittable *self, Ray *r, double ray_tmin, double ray_tmax,
+               HitRecord *rec);
+};
+
+void setFaceNormal(HitRecord *rec, Ray *r, Vector3D outward_normal);
+HitRecord createHitRecord(Point3D p, Vector3D normal, double t, int front_face);
+
+#endif // !HITTABLE_H
